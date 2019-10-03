a45883016ab5cc3ff3b7f5cd60ff446a4e16a967
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-03 15:10:26 +0800

    first commit

---
+++ src/client/App.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+class App extends React.Component<IAppProps, IAppState> {
+
+    constructor(props: IAppProps) {
+        super(props);
+        this.state = {
+            name: null
+        };
+    }
+
+    async componentWillMount() {
+        try {
+            let r = await fetch('/api/hello');
+            let name = await r.json();
+            this.setState({ name });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        return (
+            <main className="container my-5">
+                <h1 className="text-primary text-center">Hello {this.state.name}!</h1>
+            </main>
+        )
+    }
+}
+
+export interface IAppProps { }
+
+export interface IAppState {
+    name: string;
+}
+
+export default App;
